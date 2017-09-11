3300ffe611195655d7b5057433e675a4633e6eae
Nicolas Comet <nicolas.comet@gmail.com>
2017-09-11 20:13:34 +0200

    average ms result for aspectj

--- projects/fibonacci-aspectj-agent/src/test/java/fibonacci/FibonacciTest.java
+++ projects/fibonacci-aspectj-agent/src/test/java/fibonacci/FibonacciTest.java
@@ -1,21 +1,25 @@
 package fibonacci;
 
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-
-import static org.testng.Assert.*;
 
 public class FibonacciTest {
 
-    @Test
+    private long total = 0L;
+
+    @Test(invocationCount = 50)
     public void fiboTime() throws Exception {
         long start = System.nanoTime();
         Fibonacci fibonacci = new Fibonacci();
         long fibo = fibonacci.compute(42);
         long end = System.nanoTime();
-        System.out.println(end - start);
+        // an estimate time in ns
+        total += (end - start);
     }
+
+    @AfterTest
+    public void afterTest() {
+        System.out.println("average time in ms : " + ((total / 50.0) / 1000000.0));
+    }
+
 }
