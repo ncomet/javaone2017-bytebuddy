a9b932c00b2ebddfdca9497a8ed2004f9a14c470
Nicolas Comet <nicolas.comet@gmail.com>
2017-09-12 20:00:09 +0200

    english please

--- projects/bytebuddy/src/main/java/interceptors/StrangeFeelingInterceptor.java
+++ projects/bytebuddy/src/main/java/interceptors/StrangeFeelingInterceptor.java
@@ -16,7 +16,7 @@ public class StrangeFeelingInterceptor {
     public static Set<String> intercept(@SuperCall Callable<Set<String>> callable) {
         try {
             final Set<String> stomach = callable.call();
-            stomach.add("bébé alien");
+            stomach.add("baby alien");
             return stomach;
         } catch (Exception e) {
             e.printStackTrace();
