e95b01659e4a928b8ecf0612fcb7ea8f035e29d9
Nicolas Comet <nicolas.comet@gmail.com>
2017-09-12 23:20:57 +0200

    add Stopwatch

--- projects/fibonacci-spring-aop/src/main/java/org/ncomet/fibonaccispringaop/resource/FibonacciResource.java
+++ projects/fibonacci-spring-aop/src/main/java/org/ncomet/fibonaccispringaop/resource/FibonacciResource.java
@@ -3,6 +3,7 @@ package org.ncomet.fibonaccispringaop.resource;
 import org.ncomet.fibonaccispringaop.service.FibonacciService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,10 @@ public class FibonacciResource {
 
     @GetMapping
     public ResponseEntity<String> fibo(@PathVariable int n) {
-        return ResponseEntity.ok("fibonacci(" + n + ") = " + fibonacciService.compute(n));
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        long result = fibonacciService.compute(n);
+        stopWatch.stop();
+        return ResponseEntity.ok("fibonacci(" + n + ") = " + result + "<br/> It took " + stopWatch.getLastTaskTimeMillis() + "ms");
     }
 }
