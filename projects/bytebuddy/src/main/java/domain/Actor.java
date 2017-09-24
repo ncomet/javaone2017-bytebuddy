09eb51acea5e507eabb84f9228dabb3c4d9c339c
Nicolas Comet <nicolas.comet@gmail.com>
2017-09-24 13:01:24 +0200

    refacto

--- projects/bytebuddy/src/main/java/domain/Actor.java
+++ projects/bytebuddy/src/main/java/domain/Actor.java
@@ -6,10 +6,10 @@ public interface Actor {
         return String.format("Hi, I'm %s %s, and I'll play %s", firstName(), lastName(), characterName());
     }
 
-    String characterName();
-
     String firstName();
 
     String lastName();
+
+    String characterName();
 
 }
